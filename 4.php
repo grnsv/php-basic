5e7b5a4c1780cf94a7a381fcc3e22369af9af9e3
grnsv <grin.sv@gmail.com>
2021-10-06 03:51:12 +0300

    lesson-1 completed

---
+++ 4.php
@@ -0,0 +1,18 @@
+<?php
+  $title = 'Title';
+  $date = date('Y-m-d', $timestamp);
+  $hell = 'Hello😈😈😈';
+  $txt = '';
+?>
+<html>
+  <head>
+    <title><?php echo $title;?></title>
+  </head>
+  <body>
+    <h1><?php echo $hell;?></h1>
+    <p><?php echo $txt;?></p>
+    <footer>
+      <span><?php echo $date;?></span>
+    </footer>
+  </body>
+  </html>
